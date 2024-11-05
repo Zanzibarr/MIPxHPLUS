3d1e2b0c3e88ca2a898cc512a2127623e51ffba7
Zanzibarr <matteozanella2@gmail.com>
2024-11-05 13:07:15 +0100

    testing

--- code/testing/code/results_jobs.py
+++ code/testing/code/results_jobs.py
@@ -91,11 +91,103 @@ OTHER LOGS: {other}
 
     return message
 
-#TODO: Get time statistics
 def time_stats():
+
+    found_parsing_times = []
+    found_optimization_times = []
+    found_wst_times = []
+    found_build_times = []
+    found_execution_times = []
+
+    total_times = []
+
+    for folder in [utils.opt_logs_dir, utils.good_logs_dir]:
+        for file in os.listdir(folder):
+            with open(f"{folder}/{file}", "r") as f:
+                content = f.read()
+            
+            parsing_time = float(content.partition(">>  Parsing time")[2].partition("s")[0].strip())
+            opt_time = float(content.partition(">>  Optimization time")[2].partition("s")[0].strip())
+            wst_time = float(content.partition(">>  Warm-start time")[2].partition("s")[0].strip())
+            build_time = float(content.partition(">>  Build time")[2].partition("s")[0].strip())
+            exec_time = float(content.partition(">>  Exec time")[2].partition("s")[0].strip())
+
+            found_parsing_times.append(parsing_time)
+            found_optimization_times.append(opt_time)
+            found_wst_times.append(wst_time)
+            found_build_times.append(build_time)
+            found_execution_times.append(exec_time)
+            
+            total_times.append(parsing_time + opt_time + wst_time + build_time + exec_time)
+
+    timel_parsing_times = []
+    timel_optimization_times = []
+    timel_wst_times = []
+    timel_build_times = []
+
+    for file in os.listdir(utils.timelimit_logs_dir):
+        with open(f"{utils.timelimit_logs_dir}/{file}", "r") as f:
+            content = f.read()
+        
+        parsing_time = float(content.partition(">>  Parsing time")[2].partition("s")[0].strip())
+        opt_time = float(content.partition(">>  Optimization time")[2].partition("s")[0].strip())
+        wst_time = float(content.partition(">>  Warm-start time")[2].partition("s")[0].strip())
+        build_time = float(content.partition(">>  Build time")[2].partition("s")[0].strip())
+
+        timel_parsing_times.append(parsing_time)
+        timel_optimization_times.append(opt_time)
+        timel_wst_times.append(wst_time)
+        timel_build_times.append(build_time)
+
+    count_parsing = 0
+    count_optimization = 0
+    count_warm_start = 0
+    count_build = 0
+
+    total = len(os.listdir(utils.b_timelimit_logs_dir))
+
+    for file in os.listdir(utils.b_timelimit_logs_dir):
+        with open(f"{utils.b_timelimit_logs_dir}/{file}", "r") as f:
+            content = f.read()
+        
+        parsing_time = float(content.partition(">>  Parsing time")[2].partition("s")[0].strip())
+        opt_time = float(content.partition(">>  Optimization time")[2].partition("s")[0].strip())
+        wst_time = float(content.partition(">>  Warm-start time")[2].partition("s")[0].strip())
+        build_time = float(content.partition(">>  Build time")[2].partition("s")[0].strip())
+
+        if build_time == 0:
+            #if wst_time == 0:
+            if opt_time == 0:
+                if parsing_time == 0:
+                    count_parsing += 1
+                else:
+                    count_optimization += 1
+            else:
+                #count_warm_start += 1
+                count_build += 1
+            #else:
+            #    count_build += 1
+        else:
+            print(f"WTF..., {file}")        
     
     return f"""
-TIME STATISTICS TODO
+SOLUTION FOUND:
+ -> Average parsing time: {round(sum(found_parsing_times) / len(found_parsing_times), 4)}
+ -> Average optimization time: {round(sum(found_optimization_times) / len(found_optimization_times), 4)}
+ -> Average warm-start time: {round(sum(found_wst_times) / len(found_wst_times), 4)}
+ -> Average build time: {round(sum(found_build_times) / len(found_build_times), 4)}
+ -> Average cplex time: {round(sum(found_execution_times) / len(found_execution_times), 4)}
+ -> Average total time: {round(sum(total_times) / len(total_times), 4)}
+MODEL TOO SLOW:
+ -> Average parsing time: {round(sum(timel_parsing_times) / len(timel_parsing_times), 4)}
+ -> Average optimization time: {round(sum(timel_optimization_times) / len(timel_optimization_times), 4)}
+ -> Average warm-start time: {round(sum(timel_wst_times) / len(timel_wst_times), 4)}
+ -> Average build time: {round(sum(timel_build_times) / len(timel_build_times), 4)}
+BUILD TOO SLOW:
+ -> Stopped at parsing: {count_parsing} / {total} ({round(count_parsing * 100 / total, 2)})
+ -> Stopped at optimization: {count_optimization} / {total} ({round(count_optimization * 100 / total, 2)})
+ -> Stopped at warm-start: {count_warm_start} / {total} ({round(count_warm_start * 100 / total, 2)})
+ -> Stopped at build: {count_build} / {total} ({round(count_build * 100 / total, 2)})
 """
 
 if __name__ == "__main__":
