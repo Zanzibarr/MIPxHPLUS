de4c1eddef42cdfc42905b6e7e256451c1d7ae88
Zanzibarr <matteozanella2@gmail.com>
2024-12-27 12:40:41 +0100

    dynamic model testing structure

--- code/testing/code/create_jobs.py
+++ code/testing/code/create_jobs.py
@@ -21,7 +21,7 @@ instances_list = os.listdir(instances_folder)
 random.shuffle(instances_list)
 n_batches = (len(instances_list)//batch_size) + 1
 
-algs = ["imai", "rankooh", "greedy"]
+algs = ["imai", "rankooh", "greedy", "dynamic"]
 
 for alg in algs:
 
