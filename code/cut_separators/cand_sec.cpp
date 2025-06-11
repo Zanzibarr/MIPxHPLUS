8edf362d2f3b8cc6c4b4556f9548d7724dbda9d3
Zanzibarr <matteozanella2@gmail.com>
2025-06-11 15:03:22 +0200

    v2.2.0:4 - Removed testing option for candidate sec

--- code/cut_separators/cand_sec.cpp
+++ code/cut_separators/cand_sec.cpp
@@ -8,35 +8,15 @@ build_graph(const hplus::execution& exec, const hplus::instance& inst, const bin
     std::vector<std::vector<unsigned int>> graph;
     std::unordered_map<std::pair<unsigned int, unsigned int>, unsigned int, pair_hash> edge_labels;
 
-    // Facts as nodes
-    if (exec.testing) {
-        graph = std::vector<std::vector<unsigned int>>(inst.n);
-        for (unsigned int p = 0; p < inst.n; ++p) {
-            for (const auto& act_i : inst.act_with_pre[p]) {
-                if (!unreachable_actions[act_i]) continue;
-                for (const auto& q : used_first_achievers[act_i]) {
-                    graph[p].push_back(q);
-                    edge_labels[{p, q}] = inst.m + inst.fadd_cpx_start[act_i] +
-                                          std::distance(inst.actions[act_i].eff_sparse.begin(),
-                                                        std::find(inst.actions[act_i].eff_sparse.begin(), inst.actions[act_i].eff_sparse.end(), q));
-                }
-            }
-        }
-    }
-
-    // Actions as nodes
-    if (!exec.testing) {
-        graph = std::vector<std::vector<unsigned int>>(inst.m);
-        for (const auto& act_i : unreachable_actions) {
-            for (const auto& p : used_first_achievers[act_i]) {
-                for (const auto& act_j : inst.act_with_pre[p]) {
-                    if (!unreachable_actions[act_j]) continue;
-                    graph[act_i].push_back(act_j);
-                    edge_labels[{act_i, act_j}] =
-                        inst.m + inst.fadd_cpx_start[act_i] +
-                        std::distance(inst.actions[act_i].eff_sparse.begin(),
-                                      std::find(inst.actions[act_i].eff_sparse.begin(), inst.actions[act_i].eff_sparse.end(), p));
-                }
+    graph = std::vector<std::vector<unsigned int>>(inst.n);
+    for (unsigned int p = 0; p < inst.n; ++p) {
+        for (const auto& act_i : inst.act_with_pre[p]) {
+            if (!unreachable_actions[act_i]) continue;
+            for (const auto& q : used_first_achievers[act_i]) {
+                graph[p].push_back(q);
+                edge_labels[{p, q}] = inst.m + inst.fadd_cpx_start[act_i] +
+                                      std::distance(inst.actions[act_i].eff_sparse.begin(),
+                                                    std::find(inst.actions[act_i].eff_sparse.begin(), inst.actions[act_i].eff_sparse.end(), q));
             }
         }
     }
@@ -49,17 +29,6 @@ unsigned int cand_cuts::sec(CPXCALLBACKCONTEXTptr context, const hplus::executio
                             const binary_set& unreachable_actions, const std::vector<binary_set>& used_first_achievers) {
     const auto& [graph, edge_labels] = build_graph(exec, inst, unreachable_actions, used_first_achievers);
     const auto& cycles = find_cycles_unweighted(graph, edge_labels);
-    // [[DEBUG]]
-    // int counter = 0;
-    // for (const auto& adj : graph) {
-    //     if (!adj.empty()) std::cout << counter << ": ";
-    //     for (const auto x : adj) std::cout << x << " ";
-    //     if (!adj.empty()) std::cout << std::endl;
-    //     counter += adj.size();
-    // }
-    // LOG_DEBUG << edge_labels.size() << " " << cycles.size() << " " << static_cast<double>(counter) / cycles.size();
-    // int a;
-    // std::cin >> a;
     reject_with_sec_cut(context, cycles);
     return static_cast<unsigned int>(cycles.size());
 }
