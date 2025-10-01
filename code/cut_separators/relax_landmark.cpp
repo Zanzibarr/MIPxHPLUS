7485186f16376a9740eca3d32be208a52dd2d924
Matteo Zanella <matteozanella2@gmail.com>
2025-10-01 14:21:11 +0200

    v2.2.9 - Looking for a polinomial time algorithm for separating landmarks out of fractional solutions

--- code/cut_separators/relax_landmark.cpp
+++ code/cut_separators/relax_landmark.cpp
@@ -1,8 +1,11 @@
+#include <list>
+
 #include "relax_callback.hpp"
 
 [[nodiscard]]
 std::pair<bool, std::vector<unsigned int>> relax_cuts::get_violated_landmark(CPXENVptr& env, CPXLPptr& lp, const hplus::execution& exec,
                                                                              const hplus::instance& inst, const std::vector<double>& relax_point) {
+    // if (!exec.testing) {
     std::vector<int> ind(inst.m);
     std::vector<unsigned int> landmark(0);
     std::iota(ind.begin(), ind.end(), 0);
@@ -19,7 +22,11 @@ std::pair<bool, std::vector<unsigned int>> relax_cuts::get_violated_landmark(CPX
     CPX_HANDLE_CALL(CPXgetobjval(env, lp, &cutval));
 
     // If the value of the cut is greater than 1, than no landmark has been violated
-    if (cutval >= 1 - HPLUS_EPSILON) return {false, {}};
+    if (cutval >= 1 - HPLUS_EPSILON) {
+        // TODO: Remove, this is for debugging
+        // LOG_ERROR << "No landmark found...";
+        return {false, {}};
+    }
 
     // Now we know that a landmark is violated
 
@@ -35,7 +42,65 @@ std::pair<bool, std::vector<unsigned int>> relax_cuts::get_violated_landmark(CPX
         if (reach.contains(inst.actions[act_i].pre) && !reach.contains(inst.actions[act_i].eff)) landmark.push_back(act_i);
     }
 
+    // TODO: Remove, this is for debugging
+    // LOG_DEBUG << "Found violated landmark of size: " << landmark.size();
     return {true, landmark};
+    // } else {
+    //     std::vector<unsigned int> landmark;
+    //     binary_set state(inst.n);
+
+    //     std::list<unsigned int> candidates;
+    //     for (unsigned int act_i = 0; act_i < inst.m; ++act_i) {
+    //         if (inst.actions[act_i].pre_sparse.empty()) candidates.push_back(act_i);
+    //     }
+
+    //     const auto check_landmark = [&](const std::list<unsigned int>& candidates) {
+    //         double cut_value{0};
+    //         for (const auto& act_i : candidates) cut_value += relax_point[act_i];
+    //         if (cut_value >= 1)
+    //             return true;
+    //         else {
+    //             landmark = std::vector<unsigned int>(candidates.begin(), candidates.end());
+    //             return false;
+    //         }
+    //     };
+
+    //     while (!state.contains(inst.goal)) {
+    //         if (!check_landmark(candidates)) {
+    //             // TODO: Remove, this is for debugging
+    //             LOG_DEBUG << "Found violated landmark of size: " << landmark.size();
+    //             return {true, landmark};
+    //         }
+
+    //         unsigned int choice{*candidates.begin()};
+    //         double best_value{relax_point[choice]};
+    //         for (const auto& act_i : candidates) {
+    //             if (relax_point[act_i] > best_value) {
+    //                 choice = act_i;
+    //                 best_value = relax_point[act_i];
+    //             }
+    //         }
+
+    //         // add new actions to the candidates
+    //         const auto& new_state = state | inst.actions[choice].eff;
+    //         for (const auto& p : inst.actions[choice].eff_sparse) {
+    //             if (state[p]) continue;
+    //             for (const auto& act_j : inst.act_with_pre[p]) {
+    //                 if (new_state.contains(inst.actions[act_j].pre) && std::find(candidates.begin(), candidates.end(), act_j) == candidates.end())
+    //                     candidates.push_back(act_j);
+    //             }
+    //         }
+
+    //         // purge unnecessary actions from candidates
+    //         candidates.remove_if([&](unsigned int act_j) { return new_state.contains(inst.actions[act_j].eff) && !inst.fixed_actions[act_j]; });
+
+    //         state = new_state;
+    //     }
+
+    //     // TODO: Remove, this is for debugging
+    //     LOG_ERROR << "No landmark found...";
+    //     return {false, {}};
+    // }
 }
 
 [[nodiscard]]
