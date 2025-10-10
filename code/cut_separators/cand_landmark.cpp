04b23d6f3a951b0871189f901d947b512130a565
Matteo Zanella <matteozanella2@gmail.com>
2025-10-10 17:07:48 +0200

    v2.2.10 - More optimizations in landmarks extraction in candidate callback

--- code/cut_separators/cand_landmark.cpp
+++ code/cut_separators/cand_landmark.cpp
@@ -8,16 +8,17 @@
 unsigned int cand_cuts::complementary_lm(CPXCALLBACKCONTEXTptr context, const hplus::instance& inst, const binary_set& unreachable_actions,
                                          const std::vector<unsigned int>& unused_actions, const binary_set& reachable_state) {
     binary_set unapplicable{unreachable_actions}, extension(inst.m), state{reachable_state};
+    const auto& goal{inst.goal};
     for (const auto& act_i : unused_actions) {
-        ASSERT(!state.contains(inst.goal))
+        const auto& action = inst.actions[act_i];
         // If the effects of this action won't change the reachable state, just apply it
-        if (state.contains(inst.actions[act_i].eff)) {
+        if (state.contains(action.eff)) {
             extension.add(act_i);
             continue;
         }
 
         // If the action is unreachable, then it won't change the set of reachable facts -> I can add it to the extension
-        if (!state.contains(inst.actions[act_i].pre)) {
+        if (!state.contains(action.pre)) {
             extension.add(act_i);
             // Add this action to the unapplicable actions, to eventually add its effects when it'd become applicable
             unapplicable.add(act_i);
@@ -27,32 +28,47 @@ unsigned int cand_cuts::complementary_lm(CPXCALLBACKCONTEXTptr context, const hp
         // Here the action is reachable and it has new effects
 
         // If the effect of this action reaches the goal, we don't add it to the extension
-        binary_set state_sim{state | inst.actions[act_i].eff};
-        if (state_sim.contains(inst.goal)) continue;
+        binary_set state_sim{state | action.eff};
+        if (state_sim.contains(goal)) continue;
 
-        // Simulate the effects of using this action
+        std::deque<unsigned int> candidates;
+        binary_set checked(inst.m);
+        for (const auto& act_j : unapplicable) {
+            if (state_sim.contains(inst.actions[act_j].pre)) {
+                candidates.push_back(act_j);
+                checked.add(act_j);
+            }
+        }
+
         binary_set new_reachable(inst.m);
-        while (true) {
-            bool skip{true};
-            for (const auto& act_j : unapplicable) {
-                if (new_reachable[act_j]) continue;
-                // If now a (previously) unapplicable action is applicable, add its effects to the simulated state
-                if (state_sim.contains(inst.actions[act_j].pre)) {
-                    new_reachable.add(act_j);
-                    state_sim |= inst.actions[act_j].eff;
-                    skip = false;
-                    if (state_sim.contains(inst.goal)) {
-                        skip = true;
-                        break;
+        while (!candidates.empty()) {
+            const auto act_j{candidates.front()};
+            candidates.pop_front();
+
+            new_reachable.add(act_j);
+
+            if (state_sim.contains(inst.actions[act_j].eff)) continue;
+
+            const binary_set old_state = state_sim;
+            state_sim |= inst.actions[act_j].eff;
+
+            if (state_sim.contains(goal)) break;
+
+            // Add newly applicable actions
+            for (const auto& p : inst.actions[act_j].eff) {
+                if (old_state[p]) continue;
+                for (const auto& act_k : inst.act_with_pre[p]) {
+                    if (unapplicable[act_k] && state_sim.contains(inst.actions[act_k].pre) && !checked[act_k]) {
+                        candidates.push_back(act_k);
+                        checked.add(act_k);
                     }
                 }
             }
-            if (skip) break;
         }
 
         // If the new state doesn't contain the goal, then we can update the reachable state and remove the applicable actions from the previously
         // unapplicable ones
-        if (!state_sim.contains(inst.goal)) {
+        if (!state_sim.contains(goal)) {
             extension.add(act_i);
             state = state_sim;
             unapplicable -= new_reachable;
@@ -61,7 +77,9 @@ unsigned int cand_cuts::complementary_lm(CPXCALLBACKCONTEXTptr context, const hp
 
     // Compute the landmark as the set of actions that are unused, but not in the extension
     std::vector<unsigned int> landmark;
-    std::set_difference(unused_actions.begin(), unused_actions.end(), extension.begin(), extension.end(), std::back_inserter(landmark));
+    for (const auto& act_i : unused_actions) {
+        if (!extension[act_i]) landmark.push_back(act_i);
+    }
     reject_with_lm_cut(context, landmark);
     return 1;
 }
