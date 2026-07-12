c55b5ed7c79cbc7b309cfc9cbeb8a53561609aea
Matteo Zanella <matteozanella2@gmail.com>
2026-07-12 13:49:38 +0200

    chore: utils for visualizing symmetries

--- scripts/view_symm.sh
+++ scripts/view_symm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-python3 dimacs2dot.py ../code/build/test.txt g.dot
+p_flag=(-p 0)
+[ "$2" = "1" ] && p_flag=()
 
+cd ../code/build/
+./hplus -i "$1" -v "${p_flag[@]}"
+cd ../../scripts/
+python3 dimacs2dot.py ../code/build/test.txt g.dot
 dot -Tsvg g.dot -o g.svg
